a7af5807b48eac4756cd6c6646c89b09ee426f85
pannncoba <109618566+pannncoba@users.noreply.github.com>
2022-07-22 21:24:37 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -37,23 +37,23 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.vcardowner = ['6285822347348'] //ur owner number
-global.ownername = "ArullOfc" //ur owner name
-global.ytname = "YT: ARULL OFC" //ur yt chanel name
+global.vcardowner = ['18552042201'] //ur owner number
+global.ownername = "Pann" //ur owner name
+global.ytname = "TT : PANN OPBR" //ur yt chanel name
 global.socialm = "GitHub: Warikrr" //ur github or insta name
 global.location = "Indonesia, Kalimantan Selatan, Kotabaru" //ur location
 
 //bot bomdy 
-global.owner = ['6285822347348']
-global.ownertag = '6285822347348' //ur tag number
-global.botname = 'ArullBotzMD' //ur bot name
-global.linkz = "https://youtube.com/channel/UC1ikkqsPvtWuE_QonsET1yg" //your theme url which will be displayed on whatsapp
-global.websitex = "https://chat.whatsapp.com/GdXv5LxKfDPHJ4WOH2rNu5" //ur website to be displayed
-global.botscript = 'https://youtu.be/k3QkeVdAMOs' //script link
+global.owner = ['18552042201']
+global.ownertag = '18552042201' //ur tag number
+global.botname = 'Pann - MDMD' //ur bot name
+global.linkz = "https://vt.tiktok.com/ZSRN8d3Bh/" //your theme url which will be displayed on whatsapp
+global.websitex = "https://chat.whatsapp.com/InY2TbTLC663d8pbAA947O" //ur website to be displayed
+global.botscript = 'https://vt.tiktok.com/ZSRN8d3Bh/' //script link
 global.reactmoji = "🗿" //ur menu react emoji
 global.themeemoji = "😩" //ur theme emoji
 global.packname = "Sticker by" //ur sticker watermark packname
-global.author = "ArullOfc" //ur sticker watermark author
+global.author = "Pann - MD" //ur sticker watermark author
 
 //Bot theme media
 global.thum = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
@@ -62,7 +62,7 @@ global.err4r = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur error pic
 global.thumb = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
 
 //damtabase
-global.premium = ['6285822347348'] //ur premium numbers
+global.premium = ['18552042201'] //ur premium numbers
 global.ntilinkytvid = []
 global.ntilinkytch = []
 global.ntilinkig = []
